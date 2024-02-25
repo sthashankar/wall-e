181222e8e7f6629a50263ff312344dce48cfe265
Love Shrestha <love.shrestha@careerfaqs.com.au>
2024-02-26 08:35:35 +1100

    validate input sequence

--- app/Http/Controllers/RobotController.php
+++ app/Http/Controllers/RobotController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Log;
+use Validator;
 
 class RobotController extends Controller
 {
 
     public function moveAction(Request $request)
     {
+        $validate = Validator::make($request->toArray(), [
+            'command_sequence' => ['required', 'string', 'regex:/^[NESW\s]+$/u']
+        ]);
+        if ($validate->fails()) {
+            $message = json_decode($validate->messages(), true);
+            Log::info('Validator Failed for move action', $message);
+            return response()->json(['error' => $message], 400);
+        }
         return response()->json(['requested_command_sequence' => $request->input('command_sequence')]);
     }
 
--- tests/Feature/RobotMovementTest.php
+++ tests/Feature/RobotMovementTest.php
@@ -14,11 +14,42 @@ class RobotMovementTest extends TestCase
 
     public function test_user_can_access_api()
     {
-        $response = $this->post('api/robot/move', ['command_sequence' => ' N E S W']);
+        $response = $this->post('api/robot/move', ['command_sequence' => 'N E S W']);
         $response->assertStatus(200);
         $response->assertJsonStructure([
             'requested_command_sequence'
         ]);
+    }
+
+    /**
+     * @dataProvider providerWrongCommandSequence
+     * @return void
+     */
+    public function test_command_sequence_validation($sequence)
+    {
+//        $this->expectException(Exception::class);
+//        $this->expectExceptionMessage('Validation Failed');
+        $response = $this->post('api/robot/move', ['command_sequence' => $sequence]);
+        $response->assertStatus(400);
+        $response->assertJsonStructure([
+            'error'
+        ]);
+
+    }
+
+    /**
+     * @return array[]
+     */
+    public static function providerWrongCommandSequence(): array
+    {
+
+        return [
+            'command_sequences' => [
+                'T E S T',
+                'N E S  W',
+            ]
+        ];
+
     }
 
 }
